8e19feae73a7191dea445ddb39f61bdf53e15c24
daniash84 <danielle.hess84@gmail.com>
2023-09-28 20:25:24 -0700

    adding to schema

--- db/schema.sql
+++ db/schema.sql
@@ -14,6 +14,8 @@ id int NOT NULL AUTO_INCREMENT PRIMARY KEY
 title varchar(30) NOT NULL
 salary decimal 
 department_id INT
+FOREIGN KEY (department_id) REFERENCES department(department_id)
+ON DELETE SET NULL
 );
 
 create table employee (
@@ -22,4 +24,5 @@ first_name varchar(30) NOT NULL
 last_name varchar(30) NOT NULL
 role_id int NOT NULL
 manager_id int  NOT NULL
+FOREIGN KEY (role_id) REFERENCES role(role_id)
 )
